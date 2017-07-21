126fa1c3121011a05eff5c01da8e5fdaa7c8362f
Alfredo Revilla <alfredorevilla@gmail.com>
2017-07-21 13:31:18 -0500

    added some production conditions.

--- src/AlfredoRevillaBairesdevChallengeConsole/Program.cs
+++ src/AlfredoRevillaBairesdevChallengeConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using AlfredoRevillaBairesdevChallenge.Implementations;
 using System;
+using System.Linq;
 
 namespace AlfredoRevillaBairesdevChallenge
 {
@@ -7,13 +8,35 @@ namespace AlfredoRevillaBairesdevChallenge
     {
         private static void Main(string[] args)
         {
+            var targetCountries = new[] { "peru", "argentina", "venezuela", "brazil" }; //etc
+
             new Application(
                 new FileBasedContactRepository("people.in", new StringLineToContactMapper()),
                 new RankingByAdditionalOptionalConditionsMetLogic(
+                    //
+                    //  condiciones requeridas
+                    //
                     new FluentCollectionBase<Func<Contact, bool>>()
-                    .Add(o => true),
+                    //  el país debe ser latinoamericano
+                    .Add(o => targetCountries.Contains(o.Country, StringComparer.CurrentCultureIgnoreCase))
+                    //  no debe tener rol
+                    .Add(o => o.CurrentRole.IsNullOrEmpty()),
+
+                    //
+                    //  condiciones opcionales, a mas dadas más puntaje
+                    //
                     new FluentCollectionBase<Func<Contact, bool>>()
-                    .Add(o => true)),
+                    .Add(o => true)
+                    .Add(o => o.NumberOfConnections > 100)
+                    .Add(o => o.NumberOfConnections > 200)
+                    .Add(o => o.NumberOfConnections > 300)
+                    .Add(o => o.NumberOfConnections > 400)
+                    .Add(o => o.NumberOfConnections > 500)
+                    .Add(o => o.NumberOfRecommendations > 100)
+                    .Add(o => o.NumberOfRecommendations > 200)
+                    .Add(o => o.NumberOfRecommendations > 300)
+                    .Add(o => o.NumberOfRecommendations > 400)
+                    .Add(o => o.NumberOfRecommendations > 500)),
                 new WriteResultIdsToFileHandler("people.out"),
                 new ApplicationErrorHandler()).Run();
         }
