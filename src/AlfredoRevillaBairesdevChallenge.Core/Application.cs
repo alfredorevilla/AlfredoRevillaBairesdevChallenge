d30851a79196db32e95dec789041e6d2c2d796a9
Alfredo Revilla <alfredorevilla@gmail.com>
2017-07-21 14:03:18 -0500

    added result number limit (100 as default)

--- src/AlfredoRevillaBairesdevChallenge.Core/Application.cs
+++ src/AlfredoRevillaBairesdevChallenge.Core/Application.cs
@@ -1,27 +1,30 @@
 ﻿using System;
+using System.Linq;
 
 namespace AlfredoRevillaBairesdevChallenge
 {
     public class Application
     {
+        private int _maxPotentialContactsResult;
         private IApplicationErrorHandler errorHandler;
         private IResultHandler handler;
         private ILogic logic;
         private IContactRepository repository;
 
-        public Application(IContactRepository repository, ILogic logic, IResultHandler resultHandler, IApplicationErrorHandler errorHandler)
+        public Application(IContactRepository repository, ILogic logic, IResultHandler resultHandler, IApplicationErrorHandler errorHandler, int maxPotentialContactsResult = 100)
         {
             this.repository = repository;
             this.logic = logic;
             this.handler = resultHandler;
             this.errorHandler = errorHandler;
+            this._maxPotentialContactsResult = maxPotentialContactsResult;
         }
 
         public void Run()
         {
             try
             {
-                handler.HandlePotentialCustomers(this.logic.GetPotentialCustomers(this.repository.GetCustomers()));
+                handler.HandlePotentialCustomers(this.logic.GetPotentialCustomers(this.repository.GetCustomers()).Take(_maxPotentialContactsResult));
             }
             catch (Exception e)
             {
